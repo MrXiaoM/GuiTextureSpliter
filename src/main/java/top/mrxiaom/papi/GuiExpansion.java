5bbbde48d36d6b23f1d1698ee95c5fc0fbd539c3
MrXiaoM <coolxiaom95@gmail.com>
2026-05-12 14:16:03 +0800

    add configuration and supports CraftEngine

--- src/main/java/top/mrxiaom/papi/GuiExpansion.java
+++ src/main/java/top/mrxiaom/papi/GuiExpansion.java
@@ -1,11 +1,39 @@
 package top.mrxiaom.papi;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.Configurable;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 
-public class GuiExpansion extends PlaceholderExpansion {
+import java.util.HashMap;
+import java.util.Map;
+
+public class GuiExpansion extends PlaceholderExpansion implements Configurable {
+    private String imagePlaceholder;
+    private String shiftPlaceholder;
+
+    @Override
+    public boolean register() {
+        this.imagePlaceholder = getString("image-placeholder", "img_");
+        this.shiftPlaceholder = getString("shift-placeholder", "img_offset_");
+
+        return super.register();
+    }
+
+    @Override
+    public Map<String, Object> getDefaults() {
+        Map<String, Object> defaults = new HashMap<>();
+        if (isPresent("net.momirealms.craftengine.bukkit.api.CraftEngineImages")) {
+            defaults.put("image-placeholder", "image_mm_");
+            defaults.put("shift-placeholder", "shift_mm_");
+        } else if (isPresent("dev.lone.itemsadder.api.ItemsAdder")) {
+            defaults.put("image-placeholder", "img_");
+            defaults.put("shift-placeholder", "img_offset_");
+        }
+        return defaults;
+    }
+
     @NotNull
     @Override
     public String getIdentifier() {
@@ -36,27 +64,36 @@ public class GuiExpansion extends PlaceholderExpansion {
             int connectOffset = split.length >= 5 ? -Integer.parseInt(split[4]) : 0;
             StringBuilder sb = new StringBuilder();
             if (topOffset != 0) {
-                sb.append("%img_offset_").append(topOffset).append("%");
+                sb.append("%").append(shiftPlaceholder).append(topOffset).append("%");
             }
-            sb.append("%img_").append(name).append("_1%");
+            sb.append("%").append(imagePlaceholder).append(name).append("_1%");
             if (connectOffset != 0) {
-                sb.append("%img_offset_").append(connectOffset).append("%");
+                sb.append("%").append(shiftPlaceholder).append(connectOffset).append("%");
             }
-            sb.append("%img_").append(name).append("_2%");
+            sb.append("%").append(imagePlaceholder).append(name).append("_2%");
             if (bottomOffset != 0) {
-                sb.append("%img_offset_").append(bottomOffset).append("%");
+                sb.append("%").append(shiftPlaceholder).append(bottomOffset).append("%");
             }
-            sb.append("%img_").append(name).append("_3%");
+            sb.append("%").append(imagePlaceholder).append(name).append("_3%");
             if (connectOffset != 0) {
-                sb.append("%img_offset_").append(connectOffset).append("%");
+                sb.append("%").append(shiftPlaceholder).append(connectOffset).append("%");
             }
-            sb.append("%img_").append(name).append("_4%");
+            sb.append("%").append(imagePlaceholder).append(name).append("_4%");
             if (extraOffset != 0) {
-                sb.append("%img_offset_").append(extraOffset).append("%");
+                sb.append("%").append(shiftPlaceholder).append(extraOffset).append("%");
             }
             return PlaceholderAPI.setPlaceholders(p, sb.toString());
         } catch (NumberFormatException e) {
             return "WRONG NUMBER";
+        }
+    }
+
+    private static boolean isPresent(String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (Throwable ignored) {
+            return false;
         }
     }
 }
